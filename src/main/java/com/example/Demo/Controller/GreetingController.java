5fd30f1f89cfc59a794b104e588f7c6e273fe672
bharaths1513 <bharaths.1311@gmail.com>
2021-12-22 13:56:47 +0530

    added: UC1 of Greeting App

--- src/main/java/com/example/Demo/Controller/GreetingController.java
+++ src/main/java/com/example/Demo/Controller/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.Demomodel.Greeting;
+import com.example.Demo.model.Greeting;
 
 @RestController
 @RequestMapping("/greeting")
--- src/main/java/com/example/Demo/model/Greeting.java
+++ src/main/java/com/example/Demo/model/Greeting.java
@@ -1,4 +1,4 @@
-package com.example.Demomodel;
+package com.example.Demo.model;
 
 public class Greeting {
 
